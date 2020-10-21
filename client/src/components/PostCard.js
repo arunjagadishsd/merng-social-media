d2dd8f51cd0587a416faa5c66594290f18e48835
AJ <arunjagadishsd@gmail.com>
2020-10-21 23:03:38 +0530

    Add HomePage Component with recent posts

---
+++ client/src/components/PostCard.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Card, Icon, Label, Image, Button } from 'semantic-ui-react';
+import { formatRelative } from 'date-fns';
+
+const PostCard = ({
+  post: { body, is, username, createdAt, likeCount, commentCount, likes },
+}) => {
+  const likePost = () => {
+    console.log('liked');
+  };
+  const commentOnPost = () => {
+    console.log('Comment On Post');
+  };
+  const relativeDate = formatRelative(new Date(Number(createdAt)), new Date());
+  return (
+    <Card fluid>
+      <Card.Content>
+        <Image
+          floated="right"
+          size="mini"
+          src="https://react.semantic-ui.com/images/avatar/large/molly.png"
+        />
+        <Card.Header>{username}</Card.Header>
+        <Card.Meta style={{ textTransform: 'capitalize' }}>
+          {relativeDate}
+        </Card.Meta>
+        <Card.Description>{body}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Button
+          onClick={likePost}
+          basic
+          color="teal"
+          icon="heart"
+          label={{
+            basic: true,
+            color: 'teal',
+            pointing: 'left',
+            content: likeCount,
+          }}
+        />
+        <Button
+          onClick={commentOnPost}
+          basic
+          color="blue"
+          icon="comments"
+          label={{
+            basic: true,
+            color: 'blue',
+            pointing: 'left',
+            content: likeCount,
+          }}
+        />
+      </Card.Content>
+    </Card>
+  );
+};
+
+export default PostCard;
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,52 @@
 import React from 'react';
+import { gql, useQuery } from '@apollo/client';
+import { Grid } from 'semantic-ui-react';
+
+import PostCard from '../components/PostCard.js';
 
 const HomePage = () => {
+  const FETCH_POST_QUERY = gql`
+    {
+      getPosts {
+        id
+        body
+        createdAt
+        username
+        likeCount
+        likes {
+          username
+        }
+        commentCount
+        comments {
+          id
+          username
+          createdAt
+          body
+        }
+      }
+    }
+  `;
+  const { loading, error, data } = useQuery(FETCH_POST_QUERY);
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <>
+      <div className="ui segment center aligned header">
+        <h3>Recent Posts</h3>
+      </div>
+      <Grid columns={3}>
+        <Grid.Row>
+          {loading ? (
+            <h1>Loading...</h1>
+          ) : (
+            data.getPosts &&
+            data.getPosts.map((post) => (
+              <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                <PostCard post={post} />
+              </Grid.Column>
+            ))
+          )}
+        </Grid.Row>
+      </Grid>
+    </>
   );
 };
 
