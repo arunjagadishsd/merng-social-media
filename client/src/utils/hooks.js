1545b04d751a4f3ebbe68b8c8d8e0a6ca4563014
AJ <arunjagadishsd@gmail.com>
2020-10-22 12:49:48 +0530

    Add Custom hook for form events

---
+++ client/src/utils/hooks.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+const useForm = (callback, initialState = {}) => {
+  const [values, setValues] = useState(initialState);
+  const onChangeHandler = (event) => {
+    const { name, value } = event.target;
+
+    setValues({ ...values, [name]: value });
+  };
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    callback();
+  };
+  return { onChangeHandler, onSubmitHandler, values };
+};
+
+export { useForm };
